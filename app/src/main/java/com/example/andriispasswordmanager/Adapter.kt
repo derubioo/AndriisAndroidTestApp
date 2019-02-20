2da05a398a04fe55bdebb2564df1539f21e77ef6
derubioo <derubioo@gmail.com>
2019-02-19 18:31:02 -0800

    added basic recycler view

---
+++ app/src/main/java/com/example/andriispasswordmanager/Adapter.kt
@@ -0,0 +1,39 @@
+package com.example.andriispasswordmanager
+
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class Adapter(private val dataset: Array<String>) :
+    RecyclerView.Adapter<Adapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val itemRowView: ConstraintLayout) : RecyclerView.ViewHolder(itemRowView)
+
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): Adapter.MyViewHolder {
+        // create a new view
+        val itemRowView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_row, parent, false) as ConstraintLayout
+        // set the view's size, margins, paddings and layout parameters
+
+        return MyViewHolder(itemRowView)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.itemRowView.service_name.text = dataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataset.size
+}
